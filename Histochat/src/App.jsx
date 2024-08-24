d9c4372c5507e1a432443453cf771a33ad712c13
Moon <mzes0401@gmail.com>
2024-08-25 00:03:29 +0900

    name input add

--- Histochat/src/App.jsx
+++ Histochat/src/App.jsx
@@ -7,11 +7,14 @@ function App() {
   const [chatlog, setChatlog] = useState([]);
   const [data, setData] = useState("");
   
+  const [user_name, setUserName] = useState("");
   const [user_interest, setUserInterest] = useState("");
   const [user_knowledge, setUserKnowledge] = useState("");
+  const [user_name_flag, setUserNameFlag] = useState(false);
+
   const handleChat = (user1, user2) => {
     const chat = [
-      { user: "유저", message: user1 },
+      { user: user_name, message: user1 },
       { user: "세종대왕", message: user2 },
     ];
     setChatlog(chatlog.concat(chat));
@@ -37,27 +40,51 @@ function App() {
     handleClickAPICall(userInput);
   };
 
+  const handleUserNameInput = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const handleUserName = () => {
+    if (user_name === "") {
+      alert("이름을 입력해주세요");
+      
+    } else {
+      setChatlog(chatlog.concat(user_name + "\n"));
+      setUserNameFlag(true);
+    }
+  };
+
 
   const chatlogArray = chatlog.map((chat, index) => {
     return (
-      <div key={index} style={{ textAlign: chat.user === "유저" ? "left" : "right" }}>
+      <div key={index} style={{ textAlign: chat.user === user_name ? "left" : "right" }}>
         <div style={{ fontWeight: "bold", marginBottom: "5px" }}>{chat.user}</div>
-        <div style={{ background: chat.user === "유저" ? "#e6e6e6" : "#f2f2f2", padding: "10px", borderRadius: "10px", display: "inline-block" }}>{chat.message}</div>
+        <div style={{ background: chat.user === user_name ? "#e6e6e6" : "#f2f2f2", padding: "10px", borderRadius: "10px", display: "inline-block" }}>{chat.message}</div>
         <br />
       </div>
     );
   });
   return (
-    <AppConatiner>
-      <h1>Histochat</h1>
-      <div className="chatlog-container" style={{ border: "1px solid #ccc", borderRadius: "5px", padding: "10px", maxHeight: "1000px", overflowY: "scroll" }}>
-        <div className="chatlog">{chatlogArray}</div>  
-      </div>
-      <br/>
-      <div className="input-container">
-       <Userinput isloading={loading} onSubmit={handleSubmit}/>
-      </div>
-    </AppConatiner>
+    <>
+    <h1>Histochat</h1>
+      {user_name_flag ? (
+        <AppConatiner>
+          <div className="chatlog-container" style={{ border: "1px solid #ccc", borderRadius: "5px", padding: "10px", maxHeight: "1000px", overflowY: "scroll" }}>
+            <div className="chatlog">{chatlogArray}</div>  
+          </div>
+          <br/>
+          <div className="input-container">
+            <Userinput isloading={loading} onSubmit={handleSubmit}/>
+          </div>
+        </AppConatiner>
+      ) : (
+        <div>
+          <h3>이름을 입력해주세요</h3>
+          <input type="text" value={user_name} onChange={handleUserNameInput}/>
+          <button onClick={handleUserName}>입장</button>
+        </div>
+      )}
+    </>
   )
 
 }
