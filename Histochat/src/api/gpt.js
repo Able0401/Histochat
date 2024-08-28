f2650f90031b76086a0f025b76bfc7f087ba8975
Moon <mzes0401@gmail.com>
2024-08-28 11:50:41 +0900

    prompt revise

--- Histochat/src/api/gpt.js
+++ Histochat/src/api/gpt.js
@@ -33,14 +33,14 @@ export const CallGPT = async ({ prompt, pastchatlog, user_name }) => {
   const init_prompt2 = `${persona}의 목표는 나에게 ${learning_obejctive}에 대해 대화를 통해 알려주는 거야. 그리고 세부 학습 목표는 ${JSON.stringify(
     specific_learning_objective
   )}에 들어있고 각각 목표 달성 여부가 표시되어 있어. 모든 대화에서 목표들을 반드시 기억해야해. 목표들을 반드시 기억하되 말로 드러내지 말고 대화를 이어가면서 이 목표들을 달성하기 위해 대화해줘.`;
-  const init_prompt5 = `${chatlog}가 이전 대화록이야 이 이전 대화록을 반영하고 참고해서 이서 ${input}에 대한 답변을 해줘. 이미 아는 정보는 물어보면 안되.`;
+  const init_prompt5 = `${chatlog}가 이전 대화록이야 이 이전 대화록을 반영하고 참고해서 이서 ${input}에 대한 답변을 해줘. 이미 아는 정보는 물어보면 안돼. 그리고 했던 답변은 똑같이 하면 안돼.`;
   const init_prompt6 = `${chatlog}가 이전 대화록이야. 이걸 보고 ${JSON.stringify(
     specific_learning_objective
   )}가 얼마나 달성되었지 확인해보고 같은 형식으로 JSON으로 달성 여부를 표시해줘.`;
   const init_prompt7 = `${quiz}에 있는 질문들 중 값이 없는 질문들을 순서대로 물어봐줘.`;
   const init_prompt8 = `${chatlog}가 이전 대화록이야. 이걸 보고 ${JSON.stringify(
     quiz
-  )}가 얼마나 잘 답변되었는지 확인해보고, 그 정도에 따라 상, 중, 하 값을 넣어서 같은 형식으로 JSON으로 달성 여부를 표시해줘.`;
+  )}가 얼마나 잘 답변되었는지 확인해보고, 답변에 따른 학생의 이해 정도에 따라 상, 중, 하 값을 넣어서 같은 형식으로 JSON으로 달성 여부를 표시해줘.`;
 
   if (pastchatlog.length === 0) {
     const messages = [
